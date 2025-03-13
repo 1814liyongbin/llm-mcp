18871e1a7dd3d24af24586b8150a882e144ace68
liyongbin <2637230700@qq.com>
2025-03-13 22:24:35 +0800

    init

---
+++ mcp-client/src/main/java/com/haust/Main.java
@@ -0,0 +1,12 @@
+package com.haust;
+
+/**
+ * @author: liyongbin
+ * @date: 2025/3/13 22:22
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ mcp-server/src/main/java/com/haust/Main.java
@@ -0,0 +1,12 @@
+package com.haust;
+
+/**
+ * @author: liyongbin
+ * @date: 2025/3/13 22:23
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
